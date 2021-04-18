50a568e624f66b4d441dcec57724ad96eacf9dbf
Vikram K <vikmay11@gmail.com>
2021-04-18 14:57:54 +0530

    Add feature to create new blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('',views.post_list,name='post_list'),
 
     # 127.0.0.1:8000/post/2 ---> local
-    # mydjangosite.com/post/2/ ---> online
-    path('post/<int:pk>/',views.post_detail,name='post_detail')
+    # mydjangosite.com/post/2 ---> online
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
 
+    # 127.0.0.1:8000/post/new ---> local
+    # mydjangosite.com/post/new ---> online
+    path('post/new/', views.post_new, name='post_new'),
 ]
