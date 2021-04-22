b076981737d62f785f10e321a69232fbe6db0567
Vikram K <vikmay11@gmail.com>
2021-04-22 16:46:54 +0530

    Add ability to delete blog posts.

--- blog/views.py
+++ blog/views.py
@@ -60,6 +60,12 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 @login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('/', pk=post.pk)
+
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
